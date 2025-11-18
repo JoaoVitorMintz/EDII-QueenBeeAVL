f7776916706bb6a0c8549f0c5c9dc3dcd03b3225
Giovanni <giovannibgcastro@hotmail.com>
2025-11-18 18:49:47 -0300

    Atualização Arvore.AVL para inserção
    Criação do método inserção e dos métodos de ordenação para imprimir em ordem alfabética.

--- ArvoreAVL.java
+++ ArvoreAVL.java
@@ -5,118 +5,96 @@ public class ArvoreAVL {
         this.raiz = null;
     }
 
-    // Realizar comparativo entre alturas (verificar necessidade de rotacionar árvore)
-    public int max(int altura_no1, int altura_no2) {
-        return (altura_no1 > altura_no2) ? altura_no1 : altura_no2;
+    public int max(int a, int b) {
+        return (a > b) ? a : b;
     }
-    
+
     // Receber o valor da altura das sub-árvores
     public int getAlturaNo(No no) {
         return (no == null) ? -1 : no.altura;
     }
 
     public No rotacionarDireita(No no) {
-        No aux;
-        aux = no.esq;
+        No aux = no.esq;
         no.esq = aux.dir;
         aux.dir = no;
-        no.altura = this.max(this.getAlturaNo(no.dir), this.getAlturaNo(no.esq)) + 1;
-        aux.altura = this.max(this.getAlturaNo(aux.esq), no.altura) + 1;
+        no.altura = max(getAlturaNo(no.esq), getAlturaNo(no.dir)) + 1;
+        aux.altura = max(getAlturaNo(aux.esq), getAlturaNo(aux.dir)) + 1;
         return aux;
     }
 
     public No rotacionarEsquerda(No no) {
-        No aux;
-        aux = no.dir;
+        No aux = no.dir;
         no.dir = aux.esq;
         aux.esq = no;
-        no.altura = this.max(this.getAlturaNo(no.dir), this.getAlturaNo(no.esq))+1;
-        aux.altura = this.max(this.getAlturaNo(aux.dir), no.altura)+1;
+        no.altura = max(getAlturaNo(no.esq), getAlturaNo(no.dir)) + 1;
+        aux.altura = max(getAlturaNo(aux.esq), getAlturaNo(aux.dir)) + 1;
         return aux;
     }
 
     public No rotacionarEsquerdaDireita(No no) {
-        no.esq = this.rotacionarEsquerda(no.esq);
-        return this.rotacionarDireita(no);
+        no.esq = rotacionarEsquerda(no.esq);
+        return rotacionarDireita(no);
     }
 
     public No rotacionarDireitaEsquerda(No no) {
-        no.dir = this.rotacionarDireita(no.dir);
-        return this.rotacionarEsquerda(no);
+        no.dir = rotacionarDireita(no.dir);
+        return rotacionarEsquerda(no);
     }
 
     // Para proteção dos dados a serem armazenados
     public void inserir(int ID, String nome, String dataN, String dataC, String dep, String cargo, double salario) {
-        this.raiz = this.inserir(raiz, ID, nome, dataN, dataC, dep, cargo, salario);
+        this.raiz = inserir(this.raiz, ID, nome, dataN, dataC, dep, cargo, salario);
     }
 
-    // Desenvolver código para armazenar todas as variáveis
+    // Código para armazenar todas as variáveis
     private No inserir(No raiz, int ID, String nome, String dataN, String dataC, String dep, String cargo, double salario) {
-        if(raiz == null) {
-            return (new No(ID, nome, dataN, dataC, dep, cargo, salario));
+        if (raiz == null) {
+            // Aqui todas as variáveis estão disponíveis
+            return new No(ID, nome, dataN, dataC, dep, cargo, salario);
         }
-        // if(valor <= raiz.valor) {
-        //     raiz.esquerda = inserir(raiz.esquerda, valor);
-        //     if((getAlturaNo(raiz.esquerda) - getAlturaNo(raiz.direita)) == 2)
-        //         if(valor <= raiz.esquerda.valor)
-        //             raiz = rotacionarDireita(raiz);
-        //         else
-        //             raiz = rotacionarEsquerdaDireita(raiz);
-        // }
-        // else 
-        //     if(valor > raiz.valor) {
-        //       raiz.direita = inserir(raiz.direita, valor);
-        //       if((getAlturaNo(raiz.direita)-getAlturaNo(raiz.esquerda)) == 2)
-        //           if(valor > raiz.direita.valor)
-        //               raiz = rotacionarEsquerda(raiz);
-        //           else
-        //               raiz = rotacionarDireitaEsquerda(raiz);
-        //     }
-        // raiz.altura = max(getAlturaNo(raiz.esquerda), getAlturaNo(raiz.direita))+1;
+
+        // comparação por nome
+        if (nome.compareToIgnoreCase(raiz.nome) < 0) {
+            raiz.esq = inserir(raiz.esq, ID, nome, dataN, dataC, dep, cargo, salario);
+
+            if (getAlturaNo(raiz.esq) - getAlturaNo(raiz.dir) == 2) {
+                if (nome.compareToIgnoreCase(raiz.esq.nome) < 0)
+                    raiz = rotacionarDireita(raiz);
+                else
+                    raiz = rotacionarEsquerdaDireita(raiz);
+            }
+        } 
+        else if (nome.compareToIgnoreCase(raiz.nome) > 0) {
+            raiz.dir = inserir(raiz.dir, ID, nome, dataN, dataC, dep, cargo, salario);
+
+            if (getAlturaNo(raiz.dir) - getAlturaNo(raiz.esq) == 2) {
+                if (nome.compareToIgnoreCase(raiz.dir.nome) > 0)
+                    raiz = rotacionarEsquerda(raiz);
+                else
+                    raiz = rotacionarDireitaEsquerda(raiz);
+            }
+        } 
+        else {
+            // nomes duplicados vão para a direita
+            raiz.dir = inserir(raiz.dir, ID, nome, dataN, dataC, dep, cargo, salario);
+        }
+
+        raiz.altura = max(getAlturaNo(raiz.esq), getAlturaNo(raiz.dir)) + 1;
         return raiz;
     }
 
-    public void remover(int ID, String nome, String dataN, String dataC, String dep, String cargo, double salario) {
-        this.raiz = remover(this.raiz, ID, nome, dataN, dataC, dep, cargo, salario);
+    // Exibir em ordem
+    public void exibirEmOrdem() {
+        exibirEmOrdem(this.raiz);
+        System.out.println();
     }
 
-    // Desenvolver código para remover todas as variáveis
-    private No remover(No no, int ID, String nome, String dataN, String dataC, String dep, String cargo, double salario) {
-        if(no == null) {
-            return no;
-        }
-
-        // if(valor < no.valor) {
-        //     no.esquerda = remover(no.esquerda, valor );
-        // } else {
-        //    if( valor > no.valor ) {
-        //         no.direita = remover(no.direita, valor);
-        //    } else {
-        //         if(no.esquerda != null && no.direita != null ) { // Nó possuio dois filhos 
-        //             no = sucessor(no.valor);
-        //             no.direita = remover(no.direita, no.valor);
-        //         } else {
-        //             no = ( no.esquerda != null ) ? no.esquerda : no.direita;
-        //         }
-        //    }     
-        // } 
-        return no;
-    }
-
-    // TODO: VERIFICAR ESSE CÓDIGO, ESTÁ ERRADO E DEVO CORRIGIR DEPOIS!
-    public void buscarLetra(char LETRA) {
-        No no = raiz;
-        System.out.print("LISTA DE FUNCIONÁRIOS DA QUEENBEE COM A INICIAL INDICADA:\n\n");
-        while (no != null) {
-            char[] caracteres = no.nome.toCharArray();
-            if (caracteres[0] == LETRA) {
-                System.out.println("- " + no.nome + "\n");
-                no = no.dir;
-                no = no.esq;
-            }
-            no = no.dir;
-            no = no.esq;
+    private void exibirEmOrdem(No no) {
+        if (no != null) {
+            exibirEmOrdem(no.esq);
+            System.out.println(no.toString());
+            exibirEmOrdem(no.dir);
         }
     }
-    
 }
