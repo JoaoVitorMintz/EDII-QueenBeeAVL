bc32b7d7027bc263a6bac37b125004288a41297a
Giovanni <giovannibgcastro@hotmail.com>
2025-11-18 18:52:06 -0300

    Atualização No.java para inserção
    Criação de um método para imprimir os valores certinho + cria um nó alternativo para a Axion que não tem ID

--- No.java
+++ No.java
@@ -10,7 +10,7 @@ public class No {
     No dir;
     int altura;
 
-    public No (int ID, String nome, String dataN, String dataC, String dep, String cargo, double salario) {
+    public No(int ID, String nome, String dataN, String dataC, String dep, String cargo, double salario) {
         this.ID = ID;
         this.nome = nome;
         this.dataN = dataN;
@@ -18,7 +18,34 @@ public class No {
         this.dep = dep;
         this.cargo = cargo;
         this.salario = salario;
-        altura = 0;
+        this.altura = 0;
+        this.esq = null;
+        this.dir = null;
     }
-    
+
+    // construtor alternativo usado para Axion (sem ID)
+    public No(String nome, String dataN, String dataC, String dep, String cargo, double salario) {
+        this.nome = nome;
+        this.dataN = dataN;
+        this.dataC = dataC;
+        this.dep = dep;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.altura = 0;
+        this.esq = null;
+        this.dir = null;
+        this.ID = 0; // ID padrão para Axion
+    }
+
+    // Método auxiliar para exibir dados
+    @Override
+    public String toString() {
+        return "Nome: " + nome +
+               " | ID: " + ID +
+               " | Nascimento: " + dataN +
+               " | Contratação: " + dataC +
+               " | Departamento: " + dep +
+               " | Cargo: " + cargo +
+               " | Salário: R$" + salario;
+    }
 }
